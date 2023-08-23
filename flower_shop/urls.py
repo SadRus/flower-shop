26d8647fb9b029557995be124fe44adc85c0793d
SadRus <rystixx@yandex.ru>
2023-08-23 15:57:23 +0300

    Connect static and links

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -21,5 +21,13 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('card', views.card, name='card'),
+    path('catalog', views.catalog, name='catalog'),
+    path('consultation', views.consultation, name='consultation'),
+    path('order', views.order, name='order'),
+    path('order-step', views.order_step, name='order-step'),
+    path('quiz', views.quiz, name='quiz'),
+    path('quiz-step', views.quiz_step, name='quiz-step'),
+    path('result', views.result, name='result'),
 ]
