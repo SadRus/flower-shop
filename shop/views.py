75dd1be85333cd2ca38954204be4503cc8fe547f
Rostislav <rostwik@gmail.com>
2023-08-27 01:20:13 +0300

    Add payment_id

--- shop/views.py
+++ shop/views.py
@@ -96,7 +96,7 @@ def order(request, id):
                 uuid.uuid4()
             )
 
-            print(payment)
+            order.payment_id = payment.json()['id']
 
             context = {
                 'payment': payment,
